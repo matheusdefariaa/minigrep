1e851097a4b4356d6205aa02d94cd1b0f0a680e4
matheus <matheusdefaria@proton.me>
2025-10-13 01:11:27 -0300

    final

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let mut results = Vec::new();
 
-    for line in contents.lines(){
+    for line in contents.lines() {
         if line.contains(query) {
             results.push(line)
         }
@@ -9,23 +9,17 @@ pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     results
 }
 
-
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase();
     let mut results = Vec::new();
 
-    for line in contents.lines(){
+    for line in contents.lines() {
         if line.to_lowercase().contains(&query) {
             results.push(line)
         }
     }
     results
 }
-
-
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -39,7 +33,7 @@ Rust:
 safe, fast, productive.
 Pick three.
 Duct tape";
-            assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 
     #[test]
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
+use minigrep::{search, search_case_insensitive};
 use std::env;
+use std::error::Error;
 use std::fs;
 use std::process;
-use std::error::Error;
-use minigrep::{search, search_case_insensitive};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -12,7 +12,7 @@ fn main() {
         process::exit(1);
     });
 
-    if let Err(e) =  run(config) {
+    if let Err(e) = run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
@@ -23,7 +23,6 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
 
     let results = if config.ignore_case {
         search_case_insensitive(&config.query, &contents)
-
     } else {
         search(&config.query, &contents)
     };
@@ -33,7 +32,6 @@ fn run(config: Config) -> Result<(), Box<dyn Error>> {
     }
 
     Ok(())
-
 }
 
 pub struct Config {
@@ -52,6 +50,10 @@ impl Config {
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
-        Ok(Config{ query, file_path, ignore_case })
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
